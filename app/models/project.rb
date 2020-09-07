84546e1bbbd0579b868a5bb3ed6a2940b8d91cb0
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-09-07 11:11:32 +0200

    adding style to the admin area

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,13 +5,8 @@ class Project < ApplicationRecord
 
   attr_accessor :tag_names
 
-  def tag_names
-    @tag_names
-  end
-
   def tag_names=(names)
     @tag_names = names
-    binding.pry
     @tag_names.split(' ').each do |name|
       self.tags << Tag.find_or_create_by(name: name.upcase)
     end
